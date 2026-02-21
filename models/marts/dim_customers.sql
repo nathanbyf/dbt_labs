556ebbd76857f6c8be4ebb110432fa2a316d643f
Nathan Byford <nathanbyf@gmail.com>
2026-02-21 10:59:07 +0000

    referenced sources in staging ymls

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -5,7 +5,7 @@ select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 orders as (
-select * from {{ ref('stg_jaffle_shop__orders') }}
+select * from {{ ref('fct_orders') }}
 
 ),
 
@@ -24,6 +24,14 @@ customer_orders as (
 
 ),
 
+lifetime_value as (
+
+    select customer_id,
+    sum(amount) lifetime_value
+    from orders
+    group by 1
+),
+
 
 final as (
 
@@ -33,11 +41,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(lifetime_value.lifetime_value, 0) as lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join lifetime_value using (customer_id)
 
 )
 
